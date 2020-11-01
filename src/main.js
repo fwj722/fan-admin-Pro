d16738408cff0e9ec6e645cf6d7e1055b5430083
静 <1508488207@qq.com>
2020-11-01 22:36:42 +0800

    页面路由和 布局结构调整，增加svg-sprite-loader

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'normalize.css/normalize.css'
+import './icons' // icon
+// import Cookies from 'js-cookie'
+import * as filters from './filters'
+import '@/styles/index.scss' // global css
+import './styles/element-variables.scss'
+import './permission'
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
---
+++ src/views/documentation/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container documentation-container">
+    <dropdown-menu class="document-btn" :items="articleList" title="下拉动画" />
+  </div>
+</template>
+
+<script>
+import DropdownMenu from '@/components/Share/DropdownMenu'
+
+export default {
+  name: 'Documentation',
+  components: { DropdownMenu },
+  data () {
+    return {
+      articleList: [
+        { title: '下拉动画', href: '#' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.documentation-container {
+  margin: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  .document-btn {
+    flex-shrink: 0;
+    display: block;
+    cursor: pointer;
+    background: black;
+    color: white;
+    height: 60px;
+    padding: 0 16px;
+    margin: 16px;
+    line-height: 60px;
+    font-size: 20px;
+    text-align: center;
+  }
+}
+</style>
